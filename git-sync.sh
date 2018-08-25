93666f53f85dba985909f92e5600afd37c835eaa
APP技術部-羅暉智 <light@mitake.com.tw>
2018-08-25 14:49:05 +0800

    測試

--- git-sync.sh
+++ git-sync.sh
@@ -1,24 +1,27 @@
-if [[ -f $HOME/.github_token ]]
+export SOURCE=/Users/light/Desktop/RESTserver/git-sync
+
+if [[ -f $SOURCE/.github_token ]]
 then
-  export GITHUB_TOKEN=$(cat $HOME/.github_token)
+  export GITHUB_TOKEN=$(cat $SOURCE/.github_token)
 fi
 
 
-if [[ -f $HOME/.gitlab_token ]]
+if [[ -f $SOURCE/.gitlab_token ]]
 then
-  export GITLAB_API_PRIVATE_TOKEN=$(cat $HOME/.gitlab_token)
+  export GITLAB_API_PRIVATE_TOKEN=$(cat $SOURCE/.gitlab_token)
 fi
 export GITLAB_API_ENDPOINT="https://gitlab.com/api/v4"
 
 
-export GIT_USER_NAME=codexkz
+export GIT_USER_NAME=lightning11
 export GIT_REPO_NAME=Test
 
 ##  If you don't have a git repo yet
 mkdir $GIT_REPO_NAME && cd $GIT_REPO_NAME
 git init
-# hub create
+hub create
 gitlab create_project $GIT_REPO_NAME "{visibility_level: 20}"
 
 git remote set-url origin --add https://gitlab.com/${GIT_USER_NAME}/${GIT_REPO_NAME}.git
+git remote add origin-gitlab https://gitlab.com/${GIT_USER_NAME}/${GIT_REPO_NAME}.git
 git remote -v
